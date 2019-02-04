0cb669ef5bc76801b0c7dcbd33cc32ea01c88ff5
ZeromusXYZ <zeromus.xyz@gmail.com>
2019-02-04 22:57:41 +0100

    Recreated as a Visual Studio project

---
+++ res_custom/res_custom/res_custom.cpp
@@ -0,0 +1,6 @@
+// res_custom.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
---
+++ res_custom/res_custom/resource.h
@@ -0,0 +1,33 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by res_custom.rc
+//
+#define IDB_BITMAP3                     103
+#define IDB_BITMAP4                     104
+#define IDB_BITMAP5                     105
+#define IDB_BITMAP6                     106
+#define IDB_BITMAP7                     107
+#define IDB_BITMAP8                     108
+#define IDB_BITMAP9                     109
+#define IDB_BITMAP10                    110
+#define IDB_BITMAP11                    111
+#define IDB_BITMAP12                    112
+#define IDB_BITMAP13                    113
+#define IDB_BITMAP14                    114
+#define IDB_BITMAP15                    115
+#define IDB_BITMAP16                    116
+#define IDB_BITMAP17                    117
+#define IDB_BITMAP18                    118
+#define IDB_BITMAP19                    119
+#define IDR_BINARY1                     120
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        121
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
